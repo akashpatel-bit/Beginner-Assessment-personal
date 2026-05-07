839a73a473061b7e18ded378fa47981d372c81ec
akashpatel-bit <akash.patel@startappss.com>
2026-05-07 20:55:27 +0530

    Update solution3.sql

--- SQL-Challenge/Problem3/solution3.sql
+++ SQL-Challenge/Problem3/solution3.sql
@@ -1,1 +1,21 @@
--- write your solution here
+--including first test
+SELECT TEST_ID, MARKS
+FROM (
+    SELECT 
+        TEST_ID, 
+        MARKS, 
+        LAG(MARKS, 1, 0) OVER (ORDER BY TEST_ID) as prev_marks
+    FROM student_marks
+) as subquery
+WHERE MARKS > prev_marks;
+
+--excluding first test
+SELECT TEST_ID, MARKS
+FROM (
+    SELECT 
+        TEST_ID, 
+        MARKS, 
+        LAG(MARKS) OVER (ORDER BY TEST_ID) as prev_marks
+    FROM student_marks
+) as subquery
+WHERE MARKS > prev_marks;
