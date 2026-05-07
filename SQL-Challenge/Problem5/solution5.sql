c27b00768cc22fa5cd5c1f0e8dfbf227062d9617
akashpatel-bit <akash.patel@startappss.com>
2026-05-07 20:57:35 +0530

    Update solution5.sql

--- SQL-Challenge/Problem5/solution5.sql
+++ SQL-Challenge/Problem5/solution5.sql
@@ -1,1 +1,15 @@
--- write your solution here
+-- to fill the misssing values
+WITH GroupedRoles AS (
+    SELECT 
+        ROW_ID,
+        JOB_ROLE,
+        SKILLS,
+        COUNT(JOB_ROLE) OVER (ORDER BY ROW_ID) AS RoleGroup
+    FROM job_skills
+)
+SELECT 
+    ROW_ID,
+    FIRST_VALUE(JOB_ROLE) OVER (PARTITION BY RoleGroup ORDER BY ROW_ID) AS JOB_ROLE,
+    SKILLS
+FROM GroupedRoles
+ORDER BY ROW_ID;
